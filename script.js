0edc6ad387b6e4f6d47356bedc6110882dc7d023
Taylor Myrick <taylormyrick@gmail.com>
2022-12-27 23:14:35 -0600

    added slider to adjust grid size

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const div = document.createElement('div');
 grid.style.setProperty('--grid-rows', rows);
 grid.style.setProperty('--grid-cols', cols);
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+// output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  rows = this.value;
+  cols = rows;
+  grid.style.setProperty('--grid-rows', rows);
+  grid.style.setProperty('--grid-cols', cols);
+}
+
 createGrid =  () =>{
     for( let i = 0; i < rows*cols; i++){
             const div = document.createElement('div');
