a18c0fc4c9ce820fdc4235b118adfa0533b85a94
Taylor Myrick <taylormyrick@gmail.com>
2022-12-27 22:54:06 -0600

    functioning mouseover color changing

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const grid = document.querySelector('#grid');
 let rows = 16;
 let cols = 16;
 let gridArray = [];
+const div = document.createElement('div');
 grid.style.setProperty('--grid-rows', rows);
 grid.style.setProperty('--grid-cols', cols);
 
 createGrid =  () =>{
     for( let i = 0; i < rows*cols; i++){
             const div = document.createElement('div');
-            div.textContent = "a";
+            // div.textContent = "a";
             grid.appendChild(div);
-            
+            div.addEventListener('mouseover', () => setColor(event));
     }
 }
 
+
+function setColor(event){
+    event.target.style.backgroundColor = 'black';
+    // console.log(e);
+}
 createGrid();
